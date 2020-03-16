4dab91e0abc9ab5daf06f41fb2118a76d952f300
milena-mognon <milenamognon@gmail.com>
2020-03-15 23:13:21 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+console.tron.log('hello');
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});
